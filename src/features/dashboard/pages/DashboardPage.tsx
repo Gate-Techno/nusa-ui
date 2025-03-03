588f24c3e656a8c94f1c41b275814f3f5f1113ea
picerld <raficahyadi1221@gmail.com>
2025-03-04 00:30:32 +0700

    set metadata on new page

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function DashboardPage() {
   return (
     <PageContainer>
-      <HeadMetaData title="Home" metaDescription="Home page of Nusa UI" />
+      <HeadMetaData title="Dashboard" metaDescription="Dashboard page of Nusa UI" />
       <SectionContainer centerContent>
         <main className="flex items-center justify-center min-h-screen">
           <Link href={"/"}>
