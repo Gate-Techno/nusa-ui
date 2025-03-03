2f7ae27e86e7aa58a040c7c0c9e2011812d06b10
picerld <raficahyadi1221@gmail.com>
2025-03-04 00:57:42 +0700

    feat: responsive navbar with menu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -9,7 +9,48 @@ export function Navbar() {
   return (
     <div className="fixed top-0 z-50 w-full shadow-md bg-background">
       <div className="container flex items-center justify-between px-4 py-3 mx-auto lg:grid lg:grid-cols-3">
-        <h1>navbar</h1>
+        <button
+          className="block lg:hidden"
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+        >
+          {mobileMenuOpen ? (
+            // CLOSE MENU
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-x"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+          ) : (
+            // OPEN MENU
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-text"
+            >
+              <path d="M17 6.1H3" />
+              <path d="M21 12.1H3" />
+              <path d="M15.1 18H3" />
+            </svg>
+          )}
+        </button>
+        <h1>Navbar</h1>
       </div>
 
       {mobileMenuOpen && (
@@ -23,11 +64,11 @@ export function Navbar() {
               Home
             </Link>
             <Link
-              href="/questions"
+              href="/"
               className="text-lg font-medium"
               onClick={() => setMobileMenuOpen(false)}
             >
-              Start nanya 👀
+              Let&#39;s Start
             </Link>
             <Link
               href="/login"
